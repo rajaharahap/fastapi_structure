378e22b9cfb336954ad1deb6e1d4ae49820b2a5c
Muhammad Rasywan Rustan <84183220+muhrasywan@users.noreply.github.com>
2023-07-17 08:36:52 +0700

    Update db_config.py use .env

--- config/db_config.py
+++ config/db_config.py
@@ -1,35 +1,12 @@
-# dash eis
-# db_config = {
-#     "host": "103.140.207.244",
-#     "port": "5491",
-#     "db": "eis",
-#     "username": "rni",
-#     # "password": "rni@4dm1n",
-#     "password": "rni%404dm1n",
-#     "dbType": "postgres"
-# }
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
 
 db_config = {
-    # "host": "170.1.20.21",
-    # "port": "5432",
-
-    "host": "170.1.20.41",
-    "port": "3306",
-    "db": "rni2021",
-    "username": "admin",
-    "password": "rni@4dm1n",
-    # "password": "rni%404dm1n",
-    "dbType": "mysql"
+    "host": config["DB_HOST"],
+    "port": config["DB_PORT"],
+    "db": config["DB_NAME"],
+    "username": config["DB_USER"],
+    "password": config["DB_PASSWORD"],
+    "dbType": config["DB_TYPE"],
 }
-
-# configx_rni2021 = {
-#     "host": "103.140.207.233",
-#     # "host": "170.1.20.41",
-#     "port": "3306",
-#     "db": "rni2021",
-#     "username": "admin",
-#     "password": 'rni@4dm1n',
-#     "dbType": "mysql"
-# }
-
-
