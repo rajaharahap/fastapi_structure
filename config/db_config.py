39fb49f1b26231bdcfb8df8e9ab2e5a7351c8773
rajaharahap <raja.if07@gmail.com>
2023-09-04 10:20:12 +0700

    Update db_config.py

--- config/db_config.py
+++ config/db_config.py
@@ -1,12 +1,8 @@
-from dotenv import dotenv_values
-
-config = dotenv_values(".env")
-
 db_config = {
-    "host": config["DB_HOST"],
-    "port": config["DB_PORT"],
-    "db": config["DB_NAME"],
-    "username": config["DB_USER"],
-    "password": config["DB_PASSWORD"],
-    "dbType": config["DB_TYPE"],
+    "host": "xxx.xxx.xxx.xxx",
+    "port": "xxx",
+    "db": "xxxxxx",
+    "username": "xxxxx",
+    "password": "xxxxxx",
+    "dbType": "postgres/mysql",
 }
