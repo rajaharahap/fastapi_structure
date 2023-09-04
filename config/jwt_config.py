b2103c6d72f35a80536461d20ee37adde329f42f
rajaharahap <raja.if07@gmail.com>
2023-09-04 10:21:13 +0700

    Update jwt_config.py

--- config/jwt_config.py
+++ config/jwt_config.py
@@ -1,8 +1,4 @@
-from dotenv import dotenv_values
-
-config = dotenv_values(".env")
-
 jwt_profile = {
-    "key": config["JWT_KEY"],
-    "algoritma": config["JWT_ALGORITHM"],
+    "key": "xxxx",
+    "algoritma": "SHA256",
 }
